6ef7c13a10eee5184b6c843f9136cdfda4b91f18
Pyunghyuk Yoo <pyunghyuk.yoo@payhere.in>
2024-01-04 16:45:48 +0900

    12-4

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,5 @@
 use std::env;
+use std::fs;
 
 fn main() {
     let args: Vec<String> = env::args().collect();
@@ -7,4 +8,9 @@ fn main() {
 
     println!("검색어: {}", query);
     println!("대상 파일: {}", filename);
+
+    let contents = fs::read_to_string(filename)
+        .expect("파일을 읽지 못했습니다.");
+
+    println!("파일 내용:\n{}", contents);
 }
