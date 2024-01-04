26129879448d015f8d608798ad15603789175d1f
Pyunghyuk Yoo <pyunghyuk.yoo@payhere.in>
2024-01-04 16:30:11 +0900

    12-2

--- src/main.rs
+++ src/main.rs
@@ -2,5 +2,9 @@ use std::env;
 
 fn main() {
     let args: Vec<String> = env::args().collect();
-    println!("{:?}", args);
+    let query = &args[1];
+    let filename = &args[2];
+
+    println!("검색어: {}", query);
+    println!("대상 파일: {}", filename);
 }
