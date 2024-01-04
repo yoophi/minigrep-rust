0743c582a8d3721113258c2378ab398ebe1fb538
Pyunghyuk Yoo <pyunghyuk.yoo@payhere.in>
2024-01-04 17:01:22 +0900

    12-12

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,5 @@
 use std::env;
+use std::error::Error;
 use std::fs;
 use std::process;
 
@@ -15,11 +16,12 @@ fn main() {
     run(config);
 }
 
-fn run(config: Config) {
-    let contents = fs::read_to_string(config.filename)
-        .expect("파일을 읽지 못했습니다.");
+fn run(config: Config) -> Result<(), Box<dyn Error>> {
+    let contents = fs::read_to_string(config.filename)?;
 
     println!("파일 내용:\n{}", contents);
+
+    Ok(())
 }
 
 struct Config {
