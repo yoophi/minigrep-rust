77723e25b8fc0acf907a363bcafb6b3ce1018867
Pyunghyuk Yoo <pyunghyuk.yoo@payhere.in>
2024-01-04 16:48:20 +0900

    12-5

--- src/main.rs
+++ src/main.rs
@@ -3,8 +3,7 @@ use std::fs;
 
 fn main() {
     let args: Vec<String> = env::args().collect();
-    let query = &args[1];
-    let filename = &args[2];
+    let (query, filename) = parse_config(&args);
 
     println!("검색어: {}", query);
     println!("대상 파일: {}", filename);
@@ -13,4 +12,11 @@ fn main() {
         .expect("파일을 읽지 못했습니다.");
 
     println!("파일 내용:\n{}", contents);
+}
+
+fn parse_config(args: &[String]) -> (&str, &str) {
+    let query = &args[1];
+    let filename = &args[2];
+
+    (query, filename)
 }
