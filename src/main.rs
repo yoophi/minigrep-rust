ec4a163deb7b1b6d4bf8c3e6235ca12930e31e3a
Pyunghyuk Yoo <pyunghyuk.yoo@payhere.in>
2024-01-04 16:52:10 +0900

    12-6

--- src/main.rs
+++ src/main.rs
@@ -3,20 +3,25 @@ use std::fs;
 
 fn main() {
     let args: Vec<String> = env::args().collect();
-    let (query, filename) = parse_config(&args);
+    let config = parse_config(&args);
 
-    println!("검색어: {}", query);
-    println!("대상 파일: {}", filename);
+    println!("검색어: {}", config.query);
+    println!("대상 파일: {}", config.filename);
 
-    let contents = fs::read_to_string(filename)
+    let contents = fs::read_to_string(config.filename)
         .expect("파일을 읽지 못했습니다.");
 
     println!("파일 내용:\n{}", contents);
 }
 
-fn parse_config(args: &[String]) -> (&str, &str) {
-    let query = &args[1];
-    let filename = &args[2];
+struct Config {
+    query: String,
+    filename: String,
+}
 
-    (query, filename)
+fn parse_config(args: &[String]) -> Config {
+    let query = args[1].clone();
+    let filename = args[2].clone();
+
+    Config { query, filename }
 }
